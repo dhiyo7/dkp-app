6ff7fa614a1cc1de30bb394bd48c630d4f1ef1c4
Hisyam Maulana <hisyammaulana100@gmail.com>
2019-11-12 00:20:21 +0700

    Selasa, 12 November 2019

--- app/Http/Controllers/ProduksiController.php
+++ app/Http/Controllers/ProduksiController.php
@@ -18,10 +18,10 @@ class ProduksiController extends Controller
 
     public function index()
     {
-        // $pengolahans = DB::table('produksi_pengolahans')
-        //                 ->join('jenis_olahans', 'produksi_pengolahans.jenis_olahan', '=', 'jenis_olahans.id')
-        //                 ->select('produksi_pengolahans.*', 'jenis_olahans.jenis_olahan as jenis')
-        //                 ->get();
+        $pengolahans = DB::table('produksi_pengolahans')
+                        ->join('jenis_olahans', 'produksi_pengolahans.jenis_olahan', '=', 'jenis_olahans.id')
+                        ->select('produksi_pengolahans.*', 'jenis_olahans.jenis_olahan as jenis')
+                        ->get();
         return view('pages.pengolahan.index');
     }
 
